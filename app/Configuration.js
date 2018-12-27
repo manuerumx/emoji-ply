0a3b0fd9e030e4a133d35f079cd5e56c79cd23a6
Manuel González R <mgrivera@gmail.com>
2018-12-27 17:32:14 -0200

    Test Travis status

---
+++ app/Configuration.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const EnvLoader = require('./EnvLoader');
+
+class Configuration extends EnvLoader {
+  /**
+   * @param {array} services
+   */
+  constructor(...services) {
+    super();
+    this.services = services || [];
+    this.addProperty('environment', process.env.ENV || 'test');
+    this.processServicesVariables();
+  }
+
+  getEnvironment() {
+    return this.getVariable('environment');
+  }
+
+  processServicesVariables() {
+    this.services.forEach(service => this.loadAndProcessService(service));
+  }
+
+  loadAndProcessService(service) {
+    service.forEach(element => this.loadEnvFromService(element));
+  }
+
+  loadEnvFromService(service) {
+    let variable_data = service.split('=');
+    if (variable_data.length === 2) {
+      this.addProperty(variable_data[0], process.env[variable_data[0]] || variable_data[1]);
+    }
+  }
+}
+
+
+module.exports = Configuration;
