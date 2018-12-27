b686b417b098197cdc67f081d0868f2194b8827b
Manuel González R <mgrivera@gmail.com>
2018-12-27 21:33:40 -0200

    Refactor tests

--- app/tests/Main.test.js
+++ app/tests/Main.test.js
@@ -3,43 +3,45 @@
 const Main = require('../Main');
 const fs = require('fs');
 
-test('Should run the entire process', () => {
-  expect(Main.morph()).toBe('Works');
-});
+describe('Suit main file', () => {
+  test('Should run the entire process', () => {
+    expect(Main.morph()).toBe('Works');
+  });
 
-test('Should create a lock file', () => {
-  let lock_file = Main.getLockFilePath();
-  let fp = Main.ensureLockFileExists(lock_file);
+  test('Should create a lock file', () => {
+    let lock_file = Main.getLockFilePath();
+    let fp = Main.ensureLockFileExists(lock_file);
 
-  expect(fs.existsSync(lock_file)).toBeTruthy();
+    expect(fs.existsSync(lock_file)).toBeTruthy();
 
-  Main.unLockFile(fp);
-  expect(fs.existsSync(lock_file)).toBeFalsy();
-});
+    Main.unLockFile(fp);
+    expect(fs.existsSync(lock_file)).toBeFalsy();
+  });
 
-test('Should throw an error when a lock file already exists', () => {
-  let lock_file = Main.getLockFilePath();
-  let fp = Main.ensureLockFileExists(lock_file);
+  test('Should throw an error when a lock file already exists', () => {
+    let lock_file = Main.getLockFilePath();
+    let fp = Main.ensureLockFileExists(lock_file);
 
-  expect(() => {
-    Main.ensureLockFileExists(lock_file);
-  }).toThrow('Unable to lock Emoji-ply');
+    expect(() => {
+      Main.ensureLockFileExists(lock_file);
+    }).toThrow('Unable to lock Emoji-ply');
 
-  Main.unLockFile(fp);
-  expect(fs.existsSync(lock_file)).toBeFalsy();
-});
+    Main.unLockFile(fp);
+    expect(fs.existsSync(lock_file)).toBeFalsy();
+  });
 
-function deleteLockFileForTests() {
-  const lock_file_for_tests = Main.getLockFilePath();
-  if (fs.existsSync(lock_file_for_tests)) {
-    fs.unlinkSync(lock_file_for_tests);
+  function deleteLockFileForTests() {
+    const lock_file_for_tests = Main.getLockFilePath();
+    if (fs.existsSync(lock_file_for_tests)) {
+      fs.unlinkSync(lock_file_for_tests);
+    }
   }
-}
 
-beforeEach(() => {
-  deleteLockFileForTests();
-});
+  beforeEach(() => {
+    deleteLockFileForTests();
+  });
 
-afterEach(() => {
-  deleteLockFileForTests();
+  afterEach(() => {
+    deleteLockFileForTests();
+  });
 });
